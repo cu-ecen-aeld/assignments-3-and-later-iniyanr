cc6f570d229bbc6a29eccb4e596d85bbd8754a60
iniyanr <iniyanr@Ubuntu22.myguest.virtualbox.org>
2026-03-13 15:54:05 +0530

    ass5

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
+
+# Make sure these are defined correctly
 NAME=aesdsocket
 DAEMON=/usr/bin/$NAME
 PIDFILE=/var/run/$NAME.pid
 
-case "$1" in 
-	start)
-		echo -n "Starting $NAME:"
-		start-stop-daemon --start --quiet --exec $DAEMON -- -d
-		echo "OK"
-		;;
-	stop)
-		echo -n "Stopping $NAME:"
-		start-stop-daemon --stop --quiet --pidfile $PIDFILE --signal TERM  --retry 5
-		echo "OK"
-		;;
-	restart)
-		$0 stop
-		$0 start
-		;;
-	*)
-		echo "Usage : $0 {start | stop | restart}"
-
-		exit 1
+case "$1" in
+    start)
+        echo -n "Starting $NAME: "
+        # Check if the file actually exists before trying to run it
+        if [ ! -f "$DAEMON" ]; then
+            echo "FAIL: $DAEMON not found"
+            exit 1
+        fi
+        
+        # Use -x to point to the actual binary
+        start-stop-daemon -S -q -p $PIDFILE -x $DAEMON -- -d
+        echo "OK"
+        ;;
+    stop)
+        echo -n "Stopping $NAME: "
+        start-stop-daemon -K -q -p $PIDFILE
+        echo "OK"
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
 esac
-exit 0
