6e46a6190f5a0707b7dd3eb8c6820d144a04d53a
iniyanr <iniyanr@Ubuntu22.myguest.virtualbox.org>
2026-03-13 14:21:08 +0530

    assignment 5

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,25 @@
+NAME=aesdsocket
+DAEMON=/usr/bin/$NAME
+PIDFILE=/var/run/$NAME.pid
+
+case "$1" in 
+	start)
+		echo -n "Starting $NAME:"
+		start-stop-daemon --start --quiet --exec $DAEMON -- -d
+		echo "OK"
+		;;
+	stop)
+		echo -n "Stopping $NAME:"
+		start-stop-daemon --stop --quiet --pidfile $PIDFILE --signal TERM  --retry 5
+		echo "OK"
+		;;
+	restart)
+		$0 stop
+		$0 start
+		;;
+	*)
+		echo "Usage : $0 {start | stop | restart}"
+
+		exit 1
+esac
+exit 0
